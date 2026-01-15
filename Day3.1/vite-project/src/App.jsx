2f164b2da8cfa6799bf633980ab4a4763a55add5
unknown <imsonukalwar@gmail.com>
2026-01-15 19:23:09 +0530

    bassic React code

---
+++ Day3.1/vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+//  export default 
+export function Ypp(){
+    return (
+        <h1>hello coder</h1>
+    )
+};
+//named export
+export function Ypp1(){
+    return (
+        <h1>hello coder</h1>
+    )
+};
+
+export function Ypp2(){
+    return (
+        <h1>hello coder</h1>
+    )
+};
---
+++ Day3.1/vite-project/src/main.jsx
@@ -0,0 +1,13 @@
+
+import { createRoot } from 'react-dom/client'
+
+// import Ypp from './App.jsx';
+import { Ypp1,Ypp2,Ypp } from './App.jsx';
+
+createRoot(document.getElementById('root')).render(
+<>
+<Ypp/>
+<Ypp1/>
+<Ypp2/>
+</>
+);
